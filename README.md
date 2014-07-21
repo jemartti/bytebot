d2dd4f5f81662542b80b4252491067ede79c62b0
Dom Hofmann <sevenflow@gmail.com>
2014-07-21 12:07:12 -0400

    almost there

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 Bytebot
-=======
+===
 
-Bytebot is a node app which helps you build trackers. 
+Simple command line utility to help you build bytes
+
+### Using Bytebot to forward requests from the web to your development environment
+
+1. Check out this repository
+2. `$ npm link` from the repository directory
+3. `$ bytebot` from anywhere
--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var logger = console;
 var fs = require('fs');
 var express = require('express');
@@ -33,22 +35,24 @@ WebSocket.prototype.onEvent = function (name, handler) {
     this.handlers[name] = handler;
 };
 
-function start (accessToken) {
+function start (accessToken, localURL) {
 
 	// set up bytebot-local
-	var client = new WebSocket('ws://localhost:9979');
+	var client = new WebSocket('ws://trackers.one.co:9979');
 
 	// open connection
 	client.on('open', function () {
-	    logger.info('Established connection to bytebot-remote, sending authorization');
+		var sessionID = uuid.v4();
+	    logger.info('Established connection to bytebot-remote, sending session information');
+	    logger.info('Session ID: ' + sessionID)
 	    client.sendEvent('authorize', {
-	    	'path': 'http://localhost:5000/stock',
-	        'accessToken': uuid.v4()
+	    	'path': localURL,
+	        'accessToken': sessionID
 	    });
 	});
 
 	client.onEvent('authorizationComplete', function (data) {
-		logger.info('Authorized; building dummy subscription');
+		logger.info('Session started; building dummy subscription');
 	});
 
 	// when bytebot-remote asks for update from local tracker
@@ -87,7 +91,7 @@ console.log("\n\
  | _ \\ || |  _/ -_) '_ \\/ _ \\  _|\n\
  |___/\\_, |\\__\\___|_.__/\\___/\\__|\n\
       |__/                       \
-\n".bold.yellow);
+\n".white);
 
 function tryAuthorizing (tries) {
 	tries = tries || 0;
@@ -98,43 +102,83 @@ function tryAuthorizing (tries) {
 		logger.info('Invalid authorization information; try again\n');
 	}
 
-	prompt.get({
+	try {
+		var lastSession = fs.readFileSync(pwuid().dir + '/BytebotSession', {encoding: 'utf8'});
+		lastSession = JSON.parse(lastSession);
+	} catch (e) {
+
+	}
+
+	var authPrompt = {
 		properties: {
 			username: {
 				description: 'Username:'.green
 			},
 			password: {
+				hidden: true,
 				description: 'Password:'.green
 			}
 		}
-	}, function (error, results) {
-		request.post({
-			url: 'http://api.one.co:8080/api/token',
-			form: {
-				grant_type: 'password',
-				username: results.username,
-				password: results.password
-			},
-			auth: {
-				user: 'ATWDMBB6W5YZIXS7SZQJNGDQ2FCCLJRR',
-				pass: 'LWNMO23D5QDZJCTQYNDLYMSL2BN7H7XJ'
+	};
+
+	function connectWithAccessToken (accessToken) {
+		logger.info('Authorized; saving session information');
+
+		var urlPrompt = {
+			properties: {
+				localURL: {
+					description: 'Local URL:'.green
+				}
 			}
-		}, function (error, response, body) {
-			if (body) {
-				body = JSON.parse(body);
+		};
+
+		if (lastSession && lastSession.localURL) {
+			var urlString = 'Local URL (default: ' + lastSession.localURL + ')';
+			urlPrompt.properties.localURL.description = urlString.green;
+		}
+
+		prompt.get(urlPrompt, function (error, results) {
+			if (!results.localURL) {
+				results.localURL = lastSession.localURL;
 			}
 
-			if (!error && body.access_token) {
-				logger.info('Authorized; saving session information');
-				fs.writeFileSync(pwuid().dir + '/BytebotSession', JSON.stringify({'accessToken': body.access_token}, undefined, 2));
-				start(body.access_token);
-			} else if (!error) {
-				tryAuthorizing(tries + 1);
-			} else {
-				logger.info('An unexpected error occurred; exiting');
-			}
+			logger.info('Pointing bytebot-local to ' + results.localURL);
+			fs.writeFileSync(pwuid().dir + '/BytebotSession', JSON.stringify({'accessToken': accessToken, 'localURL': results.localURL}, undefined, 2));
+			start(accessToken, results.localURL);
 		});
-	});
+	}
+
+	if (!lastSession || !lastSession.accessToken) {
+		prompt.get(authPrompt, function (error, results) {
+			request.post({
+				url: 'http://api.one.co:8080/api/token',
+				form: {
+					grant_type: 'password',
+					username: results.username,
+					password: results.password
+				},
+				auth: {
+					user: 'ATWDMBB6W5YZIXS7SZQJNGDQ2FCCLJRR',
+					pass: 'LWNMO23D5QDZJCTQYNDLYMSL2BN7H7XJ'
+				}
+			}, function (error, response, body) {
+				if (body) {
+					body = JSON.parse(body);
+				}
+
+				if (!error && body.access_token) {
+					connectWithAccessToken(body.access_token);
+				} else if (!error) {
+					tryAuthorizing(tries + 1);
+				} else {
+					logger.info('An unexpected error occurred; exiting');
+				}
+			});
+		});
+	} else {
+		logger.info('Connecting with saved access token');
+		connectWithAccessToken(lastSession.accessToken);
+	}
 }
 
 tryAuthorizing();
