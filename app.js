7d595edd46a5d5e672084bdf0364ae03aa35e896
Dom Hofmann <dom@one.co>
2014-07-25 13:42:08 -0400

    Updated to support local configuration

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
+var util = require('util');
 var logger = console;
 var fs = require('fs');
 var exec = require('child_process').exec;
 var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
 var request = require('request');
 var prompt = require('prompt');
 var pwuid = require('pwuid');
@@ -16,93 +19,111 @@ keypress(process.stdin);
 var blessed = require('blessed')
   , program = blessed.program();
 
+var bot = {};
+
 var ui = null;
 
 var menus = {
-	'default': {
-		items: [
-			{
-				shortcut: 'P',
-				title: 'Control Panel'
-			},
-			{
-				shortcut: 'O',
-				title: 'Options'
-			},
-			{
-				shortcut: 'Q',
-				title: 'Quit'
-			}
-		]
-	},
-	'options': {
-		items: [
-			{
-				shortcut: 'C',
-				title: 'Configure'
-			},
-			{
-				shortcut: 'Shift-C',
-				title: 'Generate configuration'
-			},
-			{
-				shortcut: 'ESC',
-				title: 'Back'
-			}
-		]
-	}
+    'default': {
+        items: [
+            {
+                shortcut: 'D',
+                title: 'Documentation'
+            },
+            {
+                shortcut: 'O',
+                title: 'Options'
+            },
+            {
+                shortcut: 'Q',
+                title: 'Quit'
+            }
+        ]
+    },
+    'options': {
+        items: [
+            {
+                shortcut: 'C',
+                title: 'Configure this byte'
+            },
+            /*
+            {
+                shortcut: 'Shift-C',
+                title: 'Generate configuration'
+            }
+            */
+        ]
+    }
 };
 
 var menuStack = [menus['default']];
 
 var menuHandler = function (ch, key) {
 
-	var currentMenu = menuStack[menuStack.length - 1];
+    var currentMenu = menuStack[menuStack.length - 1];
 
-	if (currentMenu == menus['default']) {
-		if (key && ((key.ctrl && key.name == 'c') || key.name == 'q')) {
-			logger.info('\nExiting');
-			process.exit(0);
-		} else if (key && !key.ctrl && key.name == 'p') {
-			var child = exec('open http://www.yahoo.com', function (error, stdout, stderr) {
+    if (key && key.ctrl && key.name == 'c') {
+        logger.info('\nExiting');
+        process.exit(0);    
+    } else if (menuStack.length > 1 && key && !key.ctrl && key.name == 'escape') {
+        menuStack.pop();
+        renderMenu();
+    }
 
-			});				
-		} else if (key && !key.ctrl && key.name == 'o') {
-			menuStack.push(menus['options']);
-			renderMenu();
-		}
-	}
+    if (currentMenu == menus['default']) {
+        if (key && !key.ctrl && key.name == 'q') {
+            logger.info('\nExiting');
+            process.exit(0);        
+        } else if (key && !key.ctrl && key.name == 'o') {
+            menuStack.push(menus['options']);
+            renderMenu();
+        } else if (key && !key.ctrl && key.name == 'd') {
+            var child = exec('open https://github.com/onehq/byte-api/blob/master/README.md', function (error, stdout, stderr) {});  
+        }
+    } else if (currentMenu == menus['options']) {
+        if (key && !key.shift && key.name == 'c') {
+            var child = exec('open ' + bot.localURL + '/config#local', function (error, stdout, stderr) {});        
+        }
+    }
 
-	setTimeout(function () {
-		process.stdout.write("\033[2K\033[1D");
-	}, 0);
+    setTimeout(function () {
+        process.stdout.write("\033[2K\033[1D");
+    }, 0);
 }
 
 function renderMenu () {
-	if (!ui) {
-		process.stdin.on('keypress', menuHandler);
+    if (!ui) {
+        process.stdin.on('keypress', menuHandler);
 
-		process.stdin.setRawMode(true);
-		process.stdin.resume();
+        process.stdin.setRawMode(true);
+        process.stdin.resume();
 
-		program.hideCursor();
-		ui = new inquirer.ui.BottomBar();
-	}
+        program.hideCursor();
+        ui = new inquirer.ui.BottomBar();
+    }
 
-	var currentMenu = menuStack[menuStack.length - 1];
+    // clone menu item so injected item isn't injected to original array every time
+    var currentMenu = JSON.parse(JSON.stringify(menuStack[menuStack.length - 1]));
 
-	var items = [];
+    if (menuStack.length > 1) {
+        currentMenu.items.push({
+            shortcut: 'ESC',
+            title: 'Back'
+        });
+    }
 
-	for (var i = 0; i < currentMenu.items.length; i++) {
-		var item = currentMenu.items[i];
-		items.push('('.white + item['shortcut'].green + ')'.white + ' ' + item['title'].green.bold);
-	};
+    var items = [];
 
-	ui.updateBottomBar('\n' + items.join('  ') + '\n');
+    for (var i = 0; i < currentMenu.items.length; i++) {
+        var item = currentMenu.items[i];
+        items.push('('.white + item['shortcut'].green + ')'.white + ' ' + item['title'].green.bold);
+    };
+
+    ui.updateBottomBar('\n' + items.join('  ') + '\n');
 }
 
 function log (message) {
-	ui ? ui.log.write(message) : logger.info(message);
+    ui ? ui.log.write(message) : logger.info(message);
 }
 
 WebSocket.prototype.sendEvent = function (name, data) {
@@ -133,49 +154,56 @@ WebSocket.prototype.onEvent = function (name, handler) {
 
 function start (accessToken, localURL) {
 
-	// set up bytebot-local
-	var client = new WebSocket('ws://trackers.one.co:9979');
+    bot.localURL = localURL;
 
-	// open connection
-	client.on('open', function () {
-		var sessionID = uuid.v4();
-	    log('Established connection to bytebot-remote; sending session information');
-	    log('Session ID: ' + sessionID)
-	    client.sendEvent('authorize', {
-	    	'path': localURL,
-	        'accessToken': accessToken,
-	        'sessionID': sessionID
-	    });
-	});
+    // set up bytebot-local
+    bot.client = new WebSocket('ws://localhost:9979');
 
-	client.onEvent('authorizationComplete', function (data) {
-		log('Session started; building dummy subscription');
-		renderMenu();
-	});
+    // open connection
+    bot.client.on('open', function () {
+        var sessionID = uuid.v4();
+        log('Established connection to bytebot-remote; sending session information');
+        log('Session ID: ' + sessionID)
+        bot.client.sendEvent('authorize', {
+            'path': localURL,
+            'accessToken': accessToken,
+            'sessionID': sessionID
+        });
+    });
 
-	// when bytebot-remote asks for update from local tracker
-	client.onEvent('getResponse', function (data) {
-	    log('Requesting update from local tracker ->');
-	    log('\tPath: ' + data.path);
-	    log('\tHeaders: ' + data.headers);
-	    log('\tBody:' + data.body);
+    bot.client.onEvent('authorizationComplete', function (data) {
+        log('Session started; building dummy subscription');
+        renderMenu();
+    });
 
-	    request.post({
-	        url: data.path,
-	        headers: data.headers,
-	        body: data.body
-	    }, function (error, response, body) {
-	        if (error) {
-	            // TODO
-	        } else {
-	        	log('Received update; sending to bytebot-remote');
-	            client.sendEvent('receivedResponse', {
-	                'headers': response.headers,
-	                'body': body
-	            });
-	        }
-	    });
-	});
+    bot.client.onEvent('configUpdated', function (data) {
+        log('Configuration was updated by bytebot-remote ->');
+        log('\t' + util.inspect(data));
+    });
+
+    // when bytebot-remote asks for update from local tracker
+    bot.client.onEvent('getResponse', function (data) {
+        log('Requesting update from local tracker ->');
+        log('\tPath: ' + data.path);
+        log('\tHeaders: ' + util.inspect(data.headers));
+        log('\tBody:' + data.body);
+
+        request.post({
+            url: data.path,
+            headers: data.headers,
+            body: data.body
+        }, function (error, response, body) {
+            if (error) {
+                // TODO
+            } else {
+                log('Received update; sending to bytebot-remote');
+                bot.client.sendEvent('receivedResponse', {
+                    'headers': response.headers,
+                    'body': body
+                });
+            }
+        });
+    });
 
 }
 
@@ -192,91 +220,103 @@ console.log("\n\
 \n".white);
 
 function tryAuthorizing (tries) {
-	tries = tries || 0;
-	if (tries >= 3) {
-		log('Too many tries; exiting');
-		process.exit();
-	} else if (tries > 0) {
-		log('Invalid authorization information; try again\n');
-	}
+    tries = tries || 0;
+    if (tries >= 3) {
+        log('Too many tries; exiting');
+        process.exit();
+    } else if (tries > 0) {
+        log('Invalid authorization information; try again\n');
+    }
 
-	try {
-		var lastSession = fs.readFileSync(pwuid().dir + '/BytebotSession', {encoding: 'utf8'});
-		lastSession = JSON.parse(lastSession);
-	} catch (e) {
+    try {
+        var lastSession = fs.readFileSync(pwuid().dir + '/BytebotSession', {encoding: 'utf8'});
+        lastSession = JSON.parse(lastSession);
+    } catch (e) {
 
-	}
+    }
 
-	var authPrompt = {
-		properties: {
-			username: {
-				description: 'Username:'.green
-			},
-			password: {
-				hidden: true,
-				description: 'Password:'.green
-			}
-		}
-	};
+    var authPrompt = {
+        properties: {
+            username: {
+                description: 'Username:'.green
+            },
+            password: {
+                hidden: true,
+                description: 'Password:'.green
+            }
+        }
+    };
 
-	function connectWithAccessToken (accessToken) {
-		log('Authorized; saving session information');
+    function connectWithAccessToken (accessToken) {
+        log('Authorized; saving session information');
 
-		var urlPrompt = {
-			properties: {
-				localURL: {
-					description: 'Local URL:'.green
-				}
-			}
-		};
+        var urlPrompt = {
+            properties: {
+                localURL: {
+                    description: 'Local URL:'.green
+                }
+            }
+        };
 
-		if (lastSession && lastSession.localURL) {
-			var urlString = 'Local URL (' + lastSession.localURL + '):';
-			urlPrompt.properties.localURL.description = urlString.green;
-		}
+        if (lastSession && lastSession.localURL) {
+            var urlString = 'Local URL (' + lastSession.localURL + '):';
+            urlPrompt.properties.localURL.description = urlString.green;
+        }
 
-		prompt.get(urlPrompt, function (error, results) {
-			if (!results.localURL) {
-				results.localURL = lastSession.localURL;
-			}
+        prompt.get(urlPrompt, function (error, results) {
+            if (!results.localURL) {
+                results.localURL = lastSession.localURL;
+            }
 
-			log('Pointing bytebot-local to ' + results.localURL);
-			fs.writeFileSync(pwuid().dir + '/BytebotSession', JSON.stringify({'accessToken': accessToken, 'localURL': results.localURL}, undefined, 2));
-			start(accessToken, results.localURL);
-		});
-	}
+            log('Pointing bytebot-local to ' + results.localURL);
+            fs.writeFileSync(pwuid().dir + '/BytebotSession', JSON.stringify({'accessToken': accessToken, 'localURL': results.localURL}, undefined, 2));
+            start(accessToken, results.localURL);
 
-	if (!lastSession || !lastSession.accessToken) {
-		prompt.get(authPrompt, function (error, results) {
-			request.post({
-				url: 'http://api.one.co:8080/api/token',
-				form: {
-					grant_type: 'password',
-					username: results.username,
-					password: results.password
-				},
-				auth: {
-					user: 'ATWDMBB6W5YZIXS7SZQJNGDQ2FCCLJRR',
-					pass: 'LWNMO23D5QDZJCTQYNDLYMSL2BN7H7XJ'
-				}
-			}, function (error, response, body) {
-				if (body) {
-					body = JSON.parse(body);
-				}
+            var server = express();
+            server.use(cors());
+            server.use(bodyParser.json());
+            server.use(bodyParser.urlencoded({
+              extended: 'true'
+            }));
+            server.post('/__config', function (req, res) {
+                log('Received request for configuration update; sending to bytebot-remote');
+                bot.client.sendEvent('setConfiguration', req.body);
+            });
+            server.listen(7001);
+        });
+    }
 
-				if (!error && body.access_token) {
-					connectWithAccessToken(body.access_token);
-				} else if (!error) {
-					tryAuthorizing(tries + 1);
-				} else {
-					log('An unexpected error occurred; exiting');
-				}
-			});
-		});
-	} else {
-		log('Connecting with saved access token');
-		connectWithAccessToken(lastSession.accessToken);
-	}
+    if (!lastSession || !lastSession.accessToken) {
+        prompt.get(authPrompt, function (error, results) {
+            request.post({
+                url: 'http://api.one.co:8080/api/token',
+                form: {
+                    grant_type: 'password',
+                    username: results.username,
+                    password: results.password
+                },
+                auth: {
+                    user: 'ATWDMBB6W5YZIXS7SZQJNGDQ2FCCLJRR',
+                    pass: 'LWNMO23D5QDZJCTQYNDLYMSL2BN7H7XJ'
+                }
+            }, function (error, response, body) {
+                if (body) {
+                    body = JSON.parse(body);
+                }
+
+                if (!error && body.access_token) {
+                    connectWithAccessToken(body.access_token);
+                } else if (!error) {
+                    tryAuthorizing(tries + 1);
+                } else {
+                    log('An unexpected error occurred; exiting');
+                }
+            });
+        });
+    } else {
+        log('Connecting with saved access token');
+        connectWithAccessToken(lastSession.accessToken);
+    }
 }
 
 tryAuthorizing();
